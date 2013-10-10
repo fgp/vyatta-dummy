c8b83eaf08fc94a0f745090867d43de78140dfc6
Florian Pflug <fgp@phlo.org>
2013-10-10 02:15:30 +0200

    That update-rc.d business was harder than I thought - turns out debian uses something called insserv nowadays which determines the start-stop order from dependency information in a comment at the top of the init scripts. So deal with that correctly...

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ DATADIR=/opt/vyatta/share
 
 mkdir -p "${DESTDIR}${DATADIR}/vyatta-cfg/templates"
 cp -ar templates-cfg/* "${DESTDIR}${DATADIR}/vyatta-cfg/templates"
+
+mkdir -p "${DESTDIR}/etc/insserv/overrides"
+cp -ar debian/insserv.overrides.miredo "${DESTDIR}/etc/insserv/overrides"
