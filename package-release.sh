c38ef79f1293080e961f6b9a412495c6f914bcd0
Florian Pflug <fgp@phlo.org>
2013-10-10 17:22:58 +0200

    Improved packaging scripts

--- package-release.sh
+++ package-release.sh
@@ -7,9 +7,9 @@ if [ "$nextversion" = "" ]; then
 	echo "Usage: $0 <version>" >&2
 fi
 
+echo "Checking if the working copy contains uncommited changes..."
 test "$(git status -s | wc -l)" = "0" || exit 1
 
 git-dch --new-version="$version" --release
-git add debian/changelog
-git commit
+git commit -m "Updated changelog for release of $version" debian/changelog
 git-buildpackage --git-tag
--- package-snapshot.sh
+++ package-snapshot.sh
@@ -3,13 +3,14 @@ set -e
 
 nextversion="$1"
 
+echo "Checking if the working copy contains uncommited changes..."
 test "$(git status -s | wc -l)" = "0" || exit 1
 
 if [ "$nextversion" != "" ]; then
-	git-dch --new-version="$nextversion" --auto --snapshot
+	git-dch --new-version="$nextversion" --full --auto --snapshot
 else
-	git-dch --auto --snapshot
+	git-dch --full --auto --snapshot
 fi
-git add debian/changelog
-git commit
-git-buildpackage
+git-buildpackage --git-ignore-new; result=$?
+git checkout debian/changelog
+exit $result
